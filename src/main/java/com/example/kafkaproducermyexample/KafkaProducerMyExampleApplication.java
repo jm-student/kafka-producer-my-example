1476d6ca1acb6ba295714daf62385bea6806aaf6
Алексей <java.alexey@gmail.com>
2021-12-07 23:43:32 +0300

    1

---
+++ src/main/java/com/example/kafkaproducermyexample/KafkaProducerMyExampleApplication.java
@@ -0,0 +1,35 @@
+package com.example.kafkaproducermyexample;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@SpringBootApplication
+public class KafkaProducerMyExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaProducerMyExampleApplication.class, args);
+    }
+
+    @Bean
+    public NewTopic topic() {
+        return TopicBuilder.name("topic1")
+                .partitions(10)
+                .replicas(1)
+                .build();
+    }
+    @Bean
+    public ApplicationRunner runner(KafkaTemplate<String, String> template) {
+        return args -> {
+            template.send("topic1", "test1");
+        };
+    }
+
+// commit
+}
+
+
